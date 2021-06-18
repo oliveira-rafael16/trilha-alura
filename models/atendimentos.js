2bd0f78ea40ae166772cee6579ac1a19a4e0fc1b
Rafael <rafabaccarat@gmail.com>
2021-06-18 19:57:54 -0300

    Acertar data e resposta do servidor

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,14 +1,19 @@
+const moment = require('moment');
 const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
-    adiciona(atendimento) {
+    adiciona(atendimento, res) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss');
+        const data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        const atendimentoDatado = {...atendimento, data, dataCriacao};
+
         const sql = 'INSERT INTO Atendimentos SET ?';
 
-        conexao.query(sql, atendimento, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro){
-                console.log(erro);
+                res.json(erro);
             } else {
-                console.log(resultados);
+                res.json(resultados);
             }
         })
     }
