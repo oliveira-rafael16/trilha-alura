b29a46e9bf2f0833a5e7db1bdccdf07427ebbdc5
Rafael <rafabaccarat@gmail.com>
2021-06-18 17:38:56 -0300

    Acertando banco de dados, get e post

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const express = require('express')
+const express = require('express');
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = express()
+conexao.connect(erro => {
+    if(erro){
+        console.log(erro);
+    } else {
+        console.log('Conectado com sucesso!');
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        Tabelas.init(conexao);
+        const app = customExpress();
+
+        app.listen(3000, () => console.log('Servidor rodando'));
+
+    }
+})
