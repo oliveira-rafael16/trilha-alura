0733adb8732c07099bc33b4122319686550ae63d
Rafael <rafabaccarat@gmail.com>
2021-06-25 12:17:18 -0300

    Implementando upload de imagens e suas restricoes

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path')
 
-fs.createReadStream('./assets/salsicha.jpg')
-    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
-    .on('finish', () => console.log('Imagem foi escrita com sucesso!'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => 
+{
+    // definindo os tipos validos e o caminho
+    const tiposValidos = ['jpg', 'png', 'jpeg'];
+    const tipo = path.extname(caminho)
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1;
+    // condicional em caso o tipo seja invalido ou valido
+    if(tipoEhValido) {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(false, novoCaminho)); //callback de imagem do novoCaminho
+    } else {
+        const erro = 'Tipo é inválido';
+        console.log('Erro! Tipo inválido')
+        callbackImagemCriada(erro)
+    } 
+}
